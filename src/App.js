46c831244d00a7607d7afbb36a72a96c837fc516
Rafael <rafaeld3v@gmail.com>
2022-04-15 22:12:03 -0300

    Adicionado text-area e form-area

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ function App() {
   return (
     <div className="App">
       <main className="container">
-        <section>
-
+        <section className="text-area">
+          <h1>Learn to code by watching others</h1>
+          <p>See how experienced developers solve problems in real-time. Waching scripted tutorials is great, but understanding how developers think is invaluable.</p>
         </section>
-      </main>
+        <section className="form-area">
+          <div className="try-it">
+            <p><span>Try it free 7 days </span>then $20/mo. thereafter</p>
+
+          </div>
+        </section>
+      </main> 
 
     </div>
   );

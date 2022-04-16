33242534785bae55ec33bce4848954f7b7471150
Rafael <rafaeld3v@gmail.com>
2022-04-15 21:25:31 -0300

    Início do projeto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="container">
+        <section>
+
+        </section>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;

4b5328d2fd6ce11a6f87ed0d7fcb5fbbddf88b79
Rafael <rafaeld3v@gmail.com>
2022-04-22 23:35:22 -0300

    Estilização do formulário

--- src/App.js
+++ src/App.js
@@ -11,11 +11,17 @@ function App() {
         <section className="form-area">
           <div className="try-it">
             <p><span>Try it free 7 days </span>then $20/mo. thereafter</p>
-
           </div>
+          <form className="claim-form">
+              <input type="text" placeholder="First Name"></input>
+              <input type="text" placeholder="Last Name"></input>
+              <input type="email" placeholder="Email Address"></input>
+              <input type="password" placeholder="Password"></input>
+              <input type="submit" value="CLAIM YOUR FREE TRIAL"></input>
+              <p>By clicking the button, you are agreeing to our <a href="#">Terms and Services</a></p>
+          </form>
         </section>
       </main> 
-
     </div>
   );
 }
